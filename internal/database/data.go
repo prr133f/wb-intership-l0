255c9950fa6695d4394c20be41ca0e3cc416e3aa
prr133f <dkrilov2005@gmail.com>
2024-05-19 03:54:08 +0300

    Fix: Update db methods
    Replace GetData with GetAllData

--- internal/database/data.go
+++ internal/database/data.go
@@ -19,14 +19,24 @@ func (db *Database) SetData(orderUID string, data json.RawMessage) error {
 	return nil
 }
 
-func (db *Database) GetData(orderUID string) (json.RawMessage, error) {
-	var data json.RawMessage
-	if err := db.PG.DB.QueryRow(context.Background(), `
+func (db *Database) GetAllData() ([]json.RawMessage, error) {
+	var data []json.RawMessage
+	rows, err := db.PG.DB.Query(context.Background(), `
 	SELECT data 
-	FROM data_schema.data 
-	WHERE order_uid = $1`, orderUID).Scan(&data); err != nil {
+	FROM data_schema.data`)
+	if err != nil {
 		db.Log.Error("Unable to get data", zap.Error(err))
 		return nil, errors.WithStack(err)
+	}
+
+	for rows.Next() {
+		var d json.RawMessage
+		if err = rows.Scan(&d); err != nil {
+			db.Log.Error("Unable to scan data", zap.Error(err))
+			return nil, errors.WithStack(err)
+		}
+
+		data = append(data, d)
 	}
 
 	return data, nil
