db46398b64c7a58a4b3aa395cc09cb0c79059fbd
prr133f <dkrilov2005@gmail.com>
2024-05-20 13:44:56 +0300

    Doc: Add GoDoc

--- pkg/logger/zap.go
+++ pkg/logger/zap.go
@@ -7,6 +7,11 @@ import (
 	"go.uber.org/zap"
 )
 
+// NewZap returns a new instance of the zap.Logger.
+//
+// It creates a new zap.Logger using the zap.NewDevelopment() function.
+// If the environment variable "APP_STATUS" is set to "release",
+// it creates a new zap.Logger using the zap.NewProduction() function.
 func NewZap() *zap.Logger {
 	logger := zap.Must(zap.NewDevelopment())
 	if strings.EqualFold(os.Getenv("APP_STATUS"), "release") {
